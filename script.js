ee5125f6c7e35f257ae575e38a95b2b35c06889a
Radhika <cu9rad@gmail.com>
2022-02-02 18:42:09 -0500

    getting confirmation from user

--- script.js
+++ script.js
@@ -12,4 +12,8 @@ function generatePassword() {
     while (isNaN(length) || length < 8 || length > 128)
         length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
     
+        var uppercase = confirm("Would you like to use uppercase letters?");
+        var lowercase = confirm("Would you like to use lowercase letters?");
+        var numbers = confirm("Would you like to use numbers?");
+        var specialchar = confirm("Would you like to use special characters?");
 }

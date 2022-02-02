d5b8f7ae493e7fa8dba7a61fdf4de0137b64f421
Radhika <cu9rad@gmail.com>
2022-02-02 18:41:22 -0500

    checking user is entering valid input

--- script.js
+++ script.js
@@ -9,5 +9,7 @@ function generatePassword() {
 
     var length = Number(prompt("How many characters would you like your password to be?"));
 
+    while (isNaN(length) || length < 8 || length > 128)
+        length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
     
 }

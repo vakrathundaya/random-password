3fe8946ef848d1e50650db22effd43e4535f0c09
Radhika <cu9rad@gmail.com>
2022-02-02 18:40:19 -0500

    storing user input

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+/*function to generate new random passowrd*/
+
+function generatePassword() {
+
+    var password = "";
+
+    var length = Number(prompt("How many characters would you like your password to be?"));
+
+    
+}
